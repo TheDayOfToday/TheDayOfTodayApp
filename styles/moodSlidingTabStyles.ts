341de70bd9bd2d1ad61a2b1cdf2f2589bdd71ddd
SeoYeJin <noonesyj@naver.com>
2025-03-26 16:14:03 +0900

    feat: 스타일 추가

--- styles/moodSlidingTabStyles.ts
+++ styles/moodSlidingTabStyles.ts
@@ -1,18 +1,52 @@
 import { StyleSheet } from "react-native";
 
 export const moodSlidingTabStyles = StyleSheet.create({
+    backgroundSheet: {
+        backgroundColor: '#fff',
+    },
     sheetView: {
         padding: 25,
     },
     headerTitle: {
         fontFamily: 'Pretendard6',
         fontSize: 20,
+        color: '#000',
         textAlign: 'left',
         width: '100%',
     },
     content: {
         paddingVertical: 20,
         alignItems: 'center',
+    },
+    moodContainer: {
+        width: '100%',
+        borderTopWidth: 1,
+        borderColor: '#bbb',
+        padding: '3%',
+    },
+    moodDegreeText: {
+        fontFamily: 'Pretendard3',
+        fontSize: 18,
+    },
+    moodButtonContainer: {
+        marginVertical: '5%',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+    },
+    moodButton: {
+        flexDirection: 'row',
+        padding: '4%',
+        margin: '2%',
+        borderRadius: 30,
+        borderWidth: 1,
+    },
+    selectedMoodButton: {
+        backgroundColor: '#eee',
+    },
+    moodButtonText: {
+        fontFamily: 'Pretendard4',
+        fontSize: 14,
+        color: '#000',
     },
     submitButtonContainer: {
         alignItems: 'flex-end',
@@ -21,7 +55,7 @@ export const moodSlidingTabStyles = StyleSheet.create({
         paddingVertical: 12,
         paddingHorizontal: 40,
         borderRadius: 130,
-        backgroundColor: '#f7f7f7',
+        backgroundColor: '#f5f5f5',
     },
     submitButtonText: {
         fontFamily: 'Pretendard3',
