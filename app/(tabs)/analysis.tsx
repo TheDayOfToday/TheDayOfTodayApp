7802789398dce3c743a05fa82bd398b3eb044648
SeoYeJin <noonesyj@naver.com>
2025-04-28 11:12:13 +0900

    refactor: 불필요한 코드 제거

--- app/(tabs)/analysis.tsx
+++ app/(tabs)/analysis.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { ScrollView, View, Text } from 'react-native';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Image } from 'react-native';
 import { analysisScreenStyles } from '@/styles/analysisScreenStyles';
 
@@ -16,7 +15,6 @@ function AnalysisScreen() {
           source={require('../../assets/images/000.png')} // 0~4
           style={{ width: 150, height: 150 }}
         />
-        {/* <FontAwesome name="circle-thin" size={200} color="#191d42" /> */}
         <Text style={analysisScreenStyles.headerText}>이번 주 일기 분석</Text>
       </View>
       <View style={analysisScreenStyles.contentContainer}>
