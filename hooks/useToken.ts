f2c9df550e3c537d5c43c964db7816c004585848
SeoYeJin <noonesyj@naver.com>
2025-04-28 11:12:52 +0900

    fix: 임시 토큰 훅

--- hooks/useToken.ts
+++ hooks/useToken.ts
@@ -1,7 +1,17 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useState, useEffect } from 'react';
 
 function useToken() {
-    const token = AsyncStorage.removeItem('accessToken');
+    const [token, setToken] = useState<string | null>(null);
+
+    useEffect(() => {
+        const loadToken = async () => {
+            const storedToken = await AsyncStorage.getItem('accessToken');
+            setToken(storedToken);
+        };
+        loadToken();
+    }, []);
+
     return token;
 }
 
