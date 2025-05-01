9707e9639f02886dd9468d9b1e6e5ad0b1378677
SeoYeJin <noonesyj@naver.com>
2025-05-01 10:53:23 +0900

    feat: 일기 분석 api 연결

--- app/(tabs)/recording/daily-analysis.tsx
+++ app/(tabs)/recording/daily-analysis.tsx
@@ -1,15 +1,26 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { ScrollView, View, Text, Pressable } from "react-native";
 import { dailyAnalysisScreenStyles } from "@/styles/dailyAnalysisScreenStyles";
-import { useRouter } from "expo-router";
-
+import { useRouter, useLocalSearchParams } from "expo-router";
+import usePostAnalyze from '@/hooks/usePostAnalyze';
+import useToken from '@/hooks/useToken';
 
 function DailyAnalysisScreen() {
+  const token = useToken();
   const router = useRouter();
+  const { diaryId } = useLocalSearchParams();
+  const numericDiaryId = Number(diaryId);
+  const { mutate: analysisMutate, data, isPending} = usePostAnalyze();
 
   const handleSubmitPress = useCallback(() => {
     router.push('/(tabs)/recording');
   }, []);
+
+  useEffect(() => {
+    if (token && !isNaN(numericDiaryId)) {
+      analysisMutate({ token, diaryId: numericDiaryId });
+    }
+  }, [token, numericDiaryId]);
 
   return(
     <ScrollView
@@ -23,7 +34,9 @@ function DailyAnalysisScreen() {
       </View>
       <Text style={dailyAnalysisScreenStyles.title}>오늘의 일기 분석</Text>
       <View style={dailyAnalysisScreenStyles.resultContainer}>
-        <Text style={dailyAnalysisScreenStyles.resultText}>오늘의 일기 분석</Text>
+        <Text style={dailyAnalysisScreenStyles.resultText}>
+          {isPending ? 'Loading...' : data?.analysis}
+        </Text>
       </View>
     </ScrollView>
   );
