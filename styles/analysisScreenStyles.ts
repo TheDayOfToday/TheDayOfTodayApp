de9b073b415092d0f1f83e5649c7208ac9419b5b
SeoYeJin <noonesyj@naver.com>
2025-05-04 23:53:40 +0900

    feat: 녹음 일기, 주간 분석 UI 변경

--- styles/analysisScreenStyles.ts
+++ styles/analysisScreenStyles.ts
@@ -8,47 +8,57 @@ export const analysisScreenStyles = StyleSheet.create({
   container: {
     alignItems: 'center',
     padding: 30,
-    gap: 20,
+    paddingTop: 0,
+    gap: 10,
+  },
+  dateContainer: {
+    width: '100%',
+    height: '20%',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  dataButton: {
+    width: '15%',
+    height: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   headerContainer: {
     justifyContent: 'center',
     alignItems: 'center',
-    gap: 30,
+    gap: 20,
   },
   headerText: {
     fontFamily: 'Hakgyoansim',
     fontSize: 24,
   },
   contentContainer: {
+    width: '100%',
+    display: 'flex',
+    alignItems: 'center',
     backgroundColor: '#fafafa',
     borderColor: '#fff',
     borderWidth: 2,
     borderRadius: 15,
     padding: 25,
-  },
-  contentText: {
-    fontFamily: 'Hakgyoansim',
-    fontSize: 18,
-  },
-
-  contentTitle: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginBottom: 12,
-  },
-  contentDegree: {
-    fontSize: 16,
-    color: '#0066CC',
-    fontWeight: '600',
-    marginBottom: 8,
-  },
-  contentFeedback: {
-    fontSize: 16,
-    lineHeight: 22,
-    marginBottom: 8,
+    gap: 16,
   },
   contentDate: {
+    fontFamily: 'Hakgyoansim',
     fontSize: 14,
-    color: '#999',
+    color: '#666',
+  },
+  contentDegree: {
+    fontFamily: 'Hakgyoansim',
+    fontSize: 16,
+    color: '#0066CC',
+  },
+  contentFeedback: {
+    fontFamily: 'Hakgyoansim',
+    fontSize: 16,
+    lineHeight: 22,
   },
 });
