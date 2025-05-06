7e37251d88153e1910487211938c3759b694826b
SeoYeJin <noonesyj@naver.com>
2025-05-06 15:41:02 +0900

    refactor: 불피룡한 코드 삭제

--- app/(tabs)/recording/index.tsx
+++ app/(tabs)/recording/index.tsx
@@ -1,24 +1,13 @@
 import React from 'react';
-import { Text, View } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import SelectModeTab from '@/components/SelectModeTab';
 import { recordingScreenStyles } from '@/styles/recordingScreenStyles';
-import { useRouter } from 'expo-router';
 
 function RecordingScreen() {
-  const router = useRouter();
-
-  const handleModeSelect = (mode: string) => {
-    if( mode === 'MonologueMode') {
-      router.push('/recording/monologue');
-    } else if ( mode === 'ConversationMode' ) {
-      router.push('/recording/conversation');
-    }
-  }
 
   return (
     <GestureHandlerRootView style={recordingScreenStyles.container}>
-      <SelectModeTab selectMode={handleModeSelect} />
+      <SelectModeTab/>
     </GestureHandlerRootView>
   );
 };
