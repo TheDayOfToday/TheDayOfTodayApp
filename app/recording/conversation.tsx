7c2ed7a6745ffe8bcecf6e2b32bdcc3d1af566fa
SeoYeJin <noonesyj@naver.com>
2025-05-08 11:49:07 +0900

    feat: 대화모드 동작 구현

--- app/recording/conversation.tsx
+++ app/recording/conversation.tsx
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { SafeAreaView, View, Text, Pressable } from 'react-native';
+import { SafeAreaView, View, Text, Pressable, Modal } from 'react-native';
+import useToken from '@/hooks/useToken';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import SelectMoodTab from '@/components/SelectMoodMeterTab';
 import useConversationQuestion from '@/hooks/useConversationQuestion';
 import useConversationEnd from '@/hooks/useConversationEnd';
 import { Audio } from 'expo-av';
-import { useLocalSearchParams } from "expo-router";
+import { useLocalSearchParams, useRouter } from "expo-router";
 import LottieView from 'lottie-react-native';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { recordingScreenStyles } from '@/styles/recordingScreenStyles';
-import useToken from '@/hooks/useToken';
+import { ModalStyles } from '@/styles/modalStyles';
 
 const recordingOptions = {
   android: {
@@ -36,19 +38,23 @@ const recordingOptions = {
 function Conversation() {
   const {
     mutateAsync: questionMutate,
-    data: questionData,
-    isSuccess: questionSuccess
   } = useConversationQuestion();
   
   const {
-    mutate: conversationEndMutate,
+    mutateAsync: conversationEndMutate,
+    isSuccess,
   } = useConversationEnd();
 
   const token = useToken();
+  const router = useRouter();
   const { diaryId } = useLocalSearchParams();
   const [recording, setRecording] = useState<Audio.Recording | null>(null);
   const [isRecording, setIsRecording] = useState(false);
+  const [isRecorded, setIsRecorded] = useState(false);
+  const [recordedUri, setRecordedUri] = useState<string | null>(null);
   const [question, setQuestion] = useState('오늘의 하루는 어땠나요?');
+  const [questionButtonEnabled, setQuestionButtonEnabled] = useState(false);
+  const [showExitModal, setShowExitModal] = useState(false);
 
   const startRecording = async () => {
       try {
@@ -85,55 +91,67 @@ function Conversation() {
     };
 
   const onPressNextButton = async () => {
-    if (!recording) {
-      console.warn('녹음이 진행 중이 아닙니다.');
-      return;
-    }
-
-    const uri = await stopRecording();
-
-    if (!uri) {
-      console.warn('녹음 파일이 없습니다.');
-      return;
-    }
-
+    if (!recordedUri) return;
     try {
       const result = await questionMutate({
         token: token!,
         question,
         diaryId: Number(diaryId),
-        audioUri: uri,
+        audioUri: recordedUri,
       });
 
       if (result?.question) {
         setQuestion(result.question);
-        await startRecording();
+        setQuestionButtonEnabled(false);
+        setRecordedUri(null);
       }
     } catch (error) {
       console.error('질문 요청 중 오류 발생:', error);
     }
   };
 
-  const onPressSubmitButton = async() => {
-    if (!isRecording) {
-      console.warn('녹음 중이 아닙니다.');
+  const onPressRecordButton = async () => {
+    if (isRecording) {
+      const uri = await stopRecording();
+      if (uri) {
+        setRecordedUri(uri);
+        setQuestionButtonEnabled(true);
+        setIsRecorded(true);
+      }
+    } else {
+      setQuestionButtonEnabled(false);
+      setRecordedUri(null);
+      await startRecording();
+    }
+  };
+
+  const onPressSubmitButton = async () => {
+    if (isRecording) return;
+
+    if (!isRecorded) {
+      setShowExitModal(true);
       return;
     }
+
     const uri = await stopRecording();
+
     if (!uri) return;
 
-    // conversationEndMutate(uri);
-  }
+    try {
+      await conversationEndMutate({
+        token: token!,
+        question,
+        diaryId: Number(diaryId),
+        audioUri: uri,
+      });
+    } catch (error) {
+      console.error('대화 종료 중 오류 발생:', error);
+    }
+  };
 
-  // useEffect(() => {
-  //     const start = async () => {
-  //       await startRecording();
-  //     };
-  //     start();
-  //     return () => {
-  //       stopRecording();
-  //     }
-  //   }, []);
+  const onPressCompleteCancelButton = () => {
+    router.back();
+  }
 
   return(
     <GestureHandlerRootView>
@@ -143,10 +161,14 @@ function Conversation() {
         </View>
         <View style={recordingScreenStyles.nextButtonContainer}>
           <Pressable
-            style={recordingScreenStyles.nextButton}
+            style={[
+              recordingScreenStyles.nextButton,
+              !questionButtonEnabled || isRecording ? { opacity: 0.4 } : {}
+            ]}
             onPress={onPressNextButton}
+            disabled={!questionButtonEnabled || isRecording}
           >
-            <Text style={recordingScreenStyles.nextButtonText}>다음</Text>
+            <Text style={recordingScreenStyles.nextButtonText}>질문 받기</Text>
           </Pressable>
         </View>
         <SafeAreaView style={recordingScreenStyles.recordingContainer}>
@@ -154,21 +176,67 @@ function Conversation() {
             source={require('../../assets/RecordingAnimation.json')}
             autoPlay
             loop
-            speed={3}
+            speed={isRecording ? 5 : 0}
             style={recordingScreenStyles.lottie}
           />
         </SafeAreaView>
         <View style={recordingScreenStyles.completeButtonContainer}>
           <Pressable
-            style={recordingScreenStyles.completeButton}
+            style={recordingScreenStyles.playButton}
+            onPress={onPressRecordButton}
+          >
+            {isRecording ? (
+              <FontAwesome name="square" size={40} color="#fff" />
+            ): (
+              <FontAwesome name="circle" size={40} color="#fff" />
+            )}
+          </Pressable>
+          <Pressable
+            style={[
+              recordingScreenStyles.completeButton,
+              isRecording && { opacity: 0.4 }
+            ]}
             onPress={onPressSubmitButton}
+            disabled={isRecording}
           >
             <MaterialIcons name="call-end" size={30} color="#fff" />
           </Pressable>
         </View>
       </View>
-      {false && (
-        <SelectMoodTab diaryId={1}/>
+      {showExitModal && (
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={showExitModal}
+          onRequestClose={() => setShowExitModal(false)}
+        >
+          <View style={ModalStyles.modalOverlay}>
+            <View style={ModalStyles.modalContent}>
+              <Text style={ModalStyles.modalTitle}>종료하시겠습니까?</Text>
+              <Text style={ModalStyles.modalSubtitle}>작성된 내용이 없어 저장되지 않습니다.</Text>
+              <View style={ModalStyles.modalButtonContainer}>
+                <Pressable
+                  style={ModalStyles.finishButton}
+                  onPress={() => {
+                    onPressCompleteCancelButton();
+                    setShowExitModal(false)
+                  }
+                }>
+                  <Text style={ModalStyles.finishButtonText}>확인</Text>
+                </Pressable>
+                <Pressable
+                  style={ModalStyles.cancelButton}
+                  onPress={() => setShowExitModal(false)}
+                >
+                  <Text style={ModalStyles.cancelButtonText}>취소</Text>
+                </Pressable>
+              </View>
+            </View>
+          </View>
+        </Modal>
+      )}
+      {isSuccess && (
+        <SelectMoodTab diaryId={Number(diaryId)}/>
       )}
       {/*
       {isSuccess && data && (
--- hooks/useConversationEnd.ts
+++ hooks/useConversationEnd.ts
@@ -1,23 +1,49 @@
-import { postEndConversation } from "@/api/record";
-import { useMutation } from "@tanstack/react-query";
+import Constants from 'expo-constants';
+import { useMutation } from '@tanstack/react-query';
+import * as FileSystem from 'expo-file-system';
+import useShowToast from '@/hooks/useShowToast';
 
 interface ConversationEndProps {
   token: string;
+  question: string;
   diaryId: number;
+  audioUri: string;
 }
 
 const useConversationEnd = () => {
-  const { mutate, data, isSuccess } = useMutation({
-    mutationFn: ({
-      token, diaryId
-    }: ConversationEndProps) => postEndConversation(token, diaryId),
+  const showToast = useShowToast();
+
+  const { mutateAsync, data, isSuccess } = useMutation({
+    mutationFn: async ({
+      token,
+      question,
+      diaryId,
+      audioUri,
+    }: ConversationEndProps) => {
+      const uploadUrl = Constants.expoConfig?.extra?.API_BASE_URL + `/diary/conversation-mode/complete?question=${question}&diaryId=${diaryId}`;
+      const result = await FileSystem.uploadAsync(uploadUrl, audioUri, {
+        fieldName: 'file',
+        httpMethod: 'POST',
+        uploadType: FileSystem.FileSystemUploadType.MULTIPART,
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      if(result.status !== 200 && result.status !== 201) {
+        throw new Error('대화 실패: &{result.status}');
+      }
+
+      return JSON.parse(result.body);
+    },
     onError: (error) => {
       console.log('대화모드 일기 업로드 실패', error);
+      showToast('error', '대화 종료 실패', '대화 모드 종료에 오류가 발생하였습니다.')
     }
   });
 
   return {
-    mutate,
+    mutateAsync,
     data,
     isSuccess,
   }
