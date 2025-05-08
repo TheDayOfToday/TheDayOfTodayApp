ac2422306c8354dcfe44793b5647e868db63c400
SeoYeJin <noonesyj@naver.com>
2025-05-08 11:47:52 +0900

    feat: 대화 모드 화면 스타일 구현

--- styles/recordingScreenStyles.ts
+++ styles/recordingScreenStyles.ts
@@ -23,22 +23,42 @@ export const recordingScreenStyles = StyleSheet.create({
     message: {
         color: '#aac9ff',
         fontFamily: 'Pretendard3',
-        fontSize: 26,
+        fontSize: 28,
     },
+    // 대화 모드 질문 받기 버튼
     nextButtonContainer: {
+        display: 'flex',
         alignItems: 'flex-end',
+        alignSelf: 'center',
         paddingHorizontal: 10,
     },
     nextButton: {
-        paddingVertical: 12,
-        paddingHorizontal: 30,
-        borderRadius: 20,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 15,
+        paddingHorizontal: 20,
+        borderRadius: 30,
         backgroundColor: 'rgba(255, 255, 255, 0.2)',
     },
     nextButtonText: {
         fontFamily: 'Pretendard5',
-        fontSize: 16,
+        fontSize: 18,
         color: '#aac9ff',
+    },
+    // 대화 모드 녹음 플레이 버튼
+    playButton: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 70,
+        height: 70,
+        paddingVertical: 5,
+        paddingHorizontal: 5,
+        borderRadius: 40,
+        borderWidth: 3,
+        borderColor: 'rgba(255, 255, 255, 0.6)',
+        backgroundColor: 'rgba(255, 255, 255, 0.2)',
     },
     recordingContainer: {
         flex: 1,
@@ -49,6 +69,7 @@ export const recordingScreenStyles = StyleSheet.create({
         width: '100%',
         height: '100%',
     },
+    // 독백 종료
     submitButtonContainer: {
         alignItems: 'flex-end',
         padding: 20,
@@ -65,9 +86,13 @@ export const recordingScreenStyles = StyleSheet.create({
         fontSize: 16,
         color: '#fff',
     },
+    // 대화 마침
     completeButtonContainer: {
-        alignItems: 'center',
-        padding: 50,
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'flex-end',
+        paddingBottom: 50,
     },
     completeButton: {
         paddingVertical: 20,
