da1bbee5aa9b012296876ce152a19db5ae9ed204
SeoYeJin <noonesyj@naver.com>
2025-05-09 14:34:15 +0900

    feat: 비밀번호 변경 화면 디자인 개선

--- styles/editProfileStyles.ts
+++ styles/editProfileStyles.ts
@@ -1,22 +1,44 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-    container: { flex: 1, backgroundColor: '#fff', padding: 20 },
-    header: { fontSize: 22, fontWeight: 'bold', marginBottom: 30 },
-    label: { fontSize: 16, marginBottom: 8, color: '#333' },
-    input: {
-      borderWidth: 1,
-      borderColor: '#ddd',
-      borderRadius: 8,
-      padding: 15,
-      marginBottom: 20,
-      fontSize: 16,
-    },
-    saveButton: {
-      backgroundColor: '#5DA3FA',
-      paddingVertical: 15,
-      borderRadius: 8,
-      marginTop: 10,
-    },
-    saveButtonText: { color: '#fff', textAlign: 'center', fontSize: 16 },
-  });
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: 20,
+  },
+  headerContainer: {
+    marginBottom: '10%',
+  },
+  headerText: {
+    fontFamily: 'Pretendard8',
+    fontSize: 22,
+  },
+  contentContainer: {
+    display: 'flex',
+    gap: '8%',
+  },
+  label: {
+    lineHeight: 30,
+    fontFamily: 'Pretendard4',
+    fontSize: 16,
+    color: '#333'
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 8,
+    padding: 15,
+    fontSize: 16,
+  },
+  saveButton: {
+    backgroundColor: '#0e0c26',
+    paddingVertical: 15,
+    borderRadius: 8,
+  },
+  saveButtonText: {
+    fontFamily: 'Pretendard6',
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: 16,
+  },
+});
