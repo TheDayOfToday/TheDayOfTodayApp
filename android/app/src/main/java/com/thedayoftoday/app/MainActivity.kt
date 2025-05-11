308fc0a9dd33620f517a313f48552b46de18e376
SeoYeJin <noonesyj@naver.com>
2025-05-11 23:22:24 +0900

    refactor: 패키지 명 수정(application->app)

--- android/app/src/main/java/com/thedayoftoday/app/MainActivity.kt
+++ android/app/src/main/java/com/thedayoftoday/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.thedayoftoday.application
+package com.thedayoftoday.app
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- android/app/src/main/java/com/thedayoftoday/app/MainApplication.kt
+++ android/app/src/main/java/com/thedayoftoday/app/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.thedayoftoday.application
+package com.thedayoftoday.app
 
 import android.app.Application
 import android.content.res.Configuration
