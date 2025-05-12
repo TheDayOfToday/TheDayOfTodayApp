75116993977be521f8dfb8949ed9b846bb3d9bd9
SeoYeJin <noonesyj@naver.com>
2025-05-12 17:53:41 +0900

    빌드 4

--- app.config.ts
+++ app.config.ts
@@ -16,7 +16,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   },
   android: {
     package: 'com.thedayoftoday.app',
-    versionCode: 3,
+    versionCode: 4,
     adaptiveIcon: {
       foregroundImage: './assets/images/adaptive-icon.png',
       backgroundColor: '#ffffff',
