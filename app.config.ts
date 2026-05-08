93ea52b87b1b487dfec90a9f384eccba12d60ebd
이서준 <seojoonlee2112@gmail.com>
2026-05-09 01:32:08 +0900

    feat: VersionCode 업데이트

--- app.config.ts
+++ app.config.ts
@@ -16,7 +16,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   },
   android: {
     package: 'com.thedayoftoday.app',
-    versionCode: 12,
+    versionCode: 18,
     adaptiveIcon: {
       foregroundImage: './assets/images/adaptive-iconv3.png',
       backgroundColor: '#ffffff',
